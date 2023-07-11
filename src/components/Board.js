8f8e2338839847e0406fcc169baf501dd1158165
Sanjay Manoj <sanjaymanoj04@gmail.com>
2023-07-11 13:03:56 -0500

    Frontend complete

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profiles from './profiles.js';
+import {Leaderboard} from './database.js';
+
+export default function Board() {
+    return (
+        <div className='board'>
+            <h1 className='leaderboard'>Leaderboard</h1>
+            <Profiles Leaderboard={Leaderboard}/>
+        </div>
+    );
+}
---
+++ src/components/profiles.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './styles.css';
+
+export default function Profiles({ Leaderboard }) {
+    return (
+        <div id='profile'>
+            {Item(Leaderboard)}
+        </div>
+    );
+}
+
+function Item(data) {  
+  return (
+        <>
+            {
+                data.map((profile, index) => (
+                    <div className='flex' key={profile.id}>
+                        <div className='item'>
+                            <img id="image" src={"https://avatars.githubusercontent.com/" + profile.discord} alt=''/>
+                            <div className='name'>
+                                { index === 0 ? (
+                                    <h2 className="name text-dark">{profile.discord + " 🥇"}</h2>
+                                ) : (
+                                    index === 1 ? ( <h2 className="name text-dark">{profile.discord + " 🥈"}</h2> ) : (
+                                        index === 2 ? ( <h2 className="name text-dark">{profile.discord + " 🥉"}</h2> ) : (
+                                            <h2 className="name text-dark">{profile.discord}</h2>
+                                        )
+                                    )
+                                )}
+                            </div>
+                        </div>
+                        <div className='score'>
+                            <span>{profile.points}</span>
+                        </div>
+                    </div>
+                ))
+            }
+        </>
+    )
+}
